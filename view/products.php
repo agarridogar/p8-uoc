dc71d947c952e3ca51dd627a484de207722d1f71
Sergio Garcia <sergio.garcia.seo@gmail.com>
2019-05-07 19:54:15 +0200

    se añaden los estilos y HTML de la pagina de producto

--- view/products.php
+++ view/products.php
@@ -1,37 +1,84 @@
 <link rel="stylesheet" href="assets/css/style_products.css" />
 
-<main>
-    <div class="container">
-        <h1>Nombre producto</h1>
-        <div class="white-box">
-            <div class="header-products">
-                <div class="principal-box">
-                    <div class="images">
-                        <img src=""/>
-                    </div>
-                    <div class="excerpt">
-                        desde...
-                        desde...
-                        desde...
-                    </div>
-                </div>
-                <div class="perks">
-                    <div>plazas</div>
-                    <div>baños</div>
-                    <div>dias</div>
-                    <div>patron</div>
-                </div>
-            </div>
-            <div class="description">
-                <h3>Description</h3>
-                <p>asdasdasda</p>
-            </div>
-        </div>
-        <div class="white-box">
-            <h4>Caracteristicas</h4>
-            <p>asdada...</p>
-        </div>
-    </div>
+<main class="product-area">
+	<div>
+		<h1>Nombre producto</h1>
+		<div class="white-box column-design">
+			<div class="header-products column-design">
+				<div class="principal-box row-design">
+					<div class="images column-design">
+						<div class="principal">
+							<img src="https://assetshare.basspro.com/content/dam/bps-general-assets/web/2017/12760-search-term-landing-pages/images/tahoe-lifestyle.jpg"/>
+						</div>
+						<div class="more-img row-plain">
+							<img src="https://assetshare.basspro.com/content/dam/bps-general-assets/web/2017/12760-search-term-landing-pages/images/tahoe-lifestyle.jpg"/>
+							<img src="https://assetshare.basspro.com/content/dam/bps-general-assets/web/2017/12760-search-term-landing-pages/images/tahoe-lifestyle.jpg"/>
+							<img src="https://assetshare.basspro.com/content/dam/bps-general-assets/web/2017/12760-search-term-landing-pages/images/tahoe-lifestyle.jpg"/>
+							<img src="https://assetshare.basspro.com/content/dam/bps-general-assets/web/2017/12760-search-term-landing-pages/images/tahoe-lifestyle.jpg"/>
+						</div>
+					</div>
+					<div class="excerpt column-design">
+						<p class="price">
+							Desde 500€ / medio dia <br>
+							Desde 900€ / dia <br>
+							Desde 4000€ / semana <br>
+						</p>
+						<p>2 camarotes, 1 baño, 12 plazas, patron incluido</p>
+						<button class="btn suncoast">
+							Solicita presupuesto sin compomiso
+						</button>
+						</div>
+				</div>
+				<div class="perks row-design">
+					<div class="column-design">
+						<span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+						XX plazas
+					</div>
+					<div class="column-design">
+						<span class="glyphicon glyphicon-tint" aria-hidden="true"></span>
+						XX baños
+					</div>
+					<div class="column-design">
+						<span class="glyphicon glyphicon-time" aria-hidden="true"></span>
+						Min- X dias
+					</div>
+					<div class="column-design">
+						<span class="glyphicon glyphicon-tower" aria-hidden="true"></span>
+						Con Patron
+					</div>
+				</div>
+			</div>
+			<div class="description">
+				<h3>Description</h3>
+				<p>
+					Princess 36 con patron para 12 personas <br>
+					Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+				</p>
+			</div>
+		</div>
+		<div class="white-box">
+			<h4>Caracteristicas</h4>
+			<ul>
+				<li>Confort</li>
+				<li>Wifi e internet</li>
+				<li>Parking gratuito</li>
+				<li>Panel solar</li>
+				<li>Navegación: GPS</li>
+				<li>Sonda: Auxiliar</li>
+				<li>Piloto automático</li>
+				<li>Motor fueraborda</li>
+				<li>Hélice de pro</li>
+				<li>Altavoces exteriores</li>
+				<li>Desalinizador</li>
+				<li>placa de cocina</li>
+				<li>Nevera</li>
+				<li>Congelador</li>
+				<li>Fregadero</li>
+				<li>Barbacoa</li>
+				<li>Plancha de cocina</li>
+			</ul>
+		</div>
+	</div>
 </main>
 
 
