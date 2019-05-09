57372a4e91ad1386d67498fd32c69a0a4e44df5b
deleif <daniel.deleiva@gmail.com>
2019-05-09 19:10:31 +0200

    Vista registro

--- view/usuario/vista_registro_usuarios.php
+++ view/usuario/vista_registro_usuarios.php
@@ -1,28 +1,63 @@
-<h1>REGISTRO DE USUARIOS</h1>
+<link rel="stylesheet" href="assets/css/style_login.css" />
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
 
-<div class="row justify-content-center" >
-<form action="?c=usuario&a=registrarUsuario" method="post" enctype="multipart/form-data" >
-       
-        <div class="form-group">
-        <label>Seleccione un nombre de usuario</label>
-        <input type="text" name="nombre" value="<?php echo $user->nombre; ?>" class="form-control" placeholder="Introduzca un nombre de usuario" style="text-align: center" required />
-    </div>
+<div class="container login-container">
+            <div class="row">
+                <div class="col-md-4 col-md-offset-1  login-form-2">
+                <h3>¿Ya estás registrado?</h3>
+                    <br>
+                    
+                    <p> Para conectarte, por favor, introduce tus datos personales aquí </p>
 
-    <div class="form-group">
-        <label>Seleccione su password</label>
-        <input type="password" name="password" value="<?php echo $user->password; ?>" class="form-control" placeholder="Ingrese su password" style="text-align: center"  required />
-    </div>
+                    <form action="?c=web&a=LoginUsuario" method="post" enctype="multipart/form-data">                    
+                        <div class="form-group">
+                            <div class="text-center">                    
+                            <button class="btn btn-responsive btninter"><b>Iniciar sesión</b></button>
+                            </div>
+                        </div>        
+                     </form>
+                </div>
+                <div class="col-md-6  login-form-1">
+                <h3>Crear cuenta</h3>
+                <form action="?c=usuario&a=registrarUsuario" method="post" enctype="multipart/form-data">                    
+                    <div class="text-center"> 
+                        <span class="fa-stack fa-sm">
+                            <i class="far fa-circle fa-stack-2x"></i>
+                            <i class="fab fa-facebook-f fa-stack-1x"></i>
+                        </span>
+                        <span class="fa-stack fa-sm">
+                            <i class="far fa-circle fa-stack-2x"></i>
+                            <i class="fab fa-google-plus fa-stack-1x"></i>
+                        </span>
+                        <span class="fa-stack fa-sm">
+                            <i class="far fa-circle fa-stack-2x"></i>
+                            <i class="fab fa-linkedin fa-stack-1x"></i>
+                        </span>
+                        </div>
+                        <div class="text-center">
+                            <p><small><b>Introduce un nombre de usuario para registrate</small></b></p>
+                        </div>
+                        <div class="form-group">
+                        <input type="text" name="nombre" value="<?php echo $user->nombre; ?>" class="form-control" placeholder="Ingresa un nombre de usuario" style="text-align: center"  required/>
+                        </div>
+                        <div class="form-group">
+                        <input type="password" name="password" value="<?php echo $user->password; ?>" class="form-control" placeholder="Ingresa un password" style="text-align: center" required/>
+                        </div>
+                        <div class="form-group">
+                        <input type="email" name="email" value="<?php echo $user->email; ?>" class="form-control" placeholder="Ingresa un email" style="text-align: center" required/>
+                        </div>
+                        <div class="form-group">
+                            <div class="text-center">             
+                                <button class="btn btn-warning btn-responsive btninter"><b>Registrarse</b></button>
+                            </div>
+                        </div>
 
-    <div class="form-group">
-        <label>Introduzca un email</label>
-        <input type="email" name="email" value="<?php echo $user->email; ?>" class="form-control" placeholder="Ingrese su email" style="text-align: center" required />
-    </div>
-    <hr/>
-    <div class="text-center">
-        <button class="btn btn-success">Guardar</button>
-    </div>
-</form>
+                    </form>
+                </div>
+        </div>
 </div>
+</div>
+
 
 <?php
     if(isset($_SESSION["errorvalidacion"])){
@@ -32,3 +67,6 @@
         session_unset();
     }
 ?>
+
+
+
